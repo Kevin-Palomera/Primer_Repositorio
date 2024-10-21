1b40f3bce5579b10fec98e301cf828d0bd6ca697
Kevin-Palomera <kevinpalomera2008@gmail.com>
2024-10-21 09:31:38 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,14 @@
+<?
+/* CBTIS 89
+P00Presentacion
+Programa contiene mis datos personales 
+Kevin Anthony Palomera Gamiz
+3°A Programacion Matutino */
+
+echo "Mi nombre es Kevin Anthony Palomera Gamiz";
+echo "<br>","Estudio en el CBTIS 89";
+echo "<br>","En tercer semestre de la carrera de programacion A Matutino";
+echo "<br>","Vivo en la ciudad de Durango, Dgo";
+
+
+?> 
