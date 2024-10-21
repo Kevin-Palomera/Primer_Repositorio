c2e8c64a32bdb64ee1d10750d1d5556db36025e7
Kevin-Palomera <kevinpalomera2008@gmail.com>
2024-10-21 09:10:12 -0600

    Add files via upload

---
+++ P01Variable1.php
@@ -0,0 +1,19 @@
+<?php
+$dia = 24; //Se eclara una variable tipo integer.
+$sueldo = 758.43; //Se declara una variable tipo double.
+$nombre = " Kevin "; //Se declara una variable de tipo string.
+$exite = true; //Se declara una variable boolean.
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre; echo "<br>";
+echo "Variable boolean";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "Mundo";
+echo $cadena1. "   ".$cadena2;
+?>
