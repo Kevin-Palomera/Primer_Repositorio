61f6cabba1ad862c84d8ca1760eefe93866f3c8c
Kevin-Palomera <kevinpalomera2008@gmail.com>
2024-11-20 09:27:16 -0700

    Add files via upload

---
+++ Exa3PalomeraGamiz.php
@@ -0,0 +1,38 @@
+<? 
+/*
+Examen3PalomeraGamiz.php
+CBTIS 89
+Programa que almacena el nombre de cuatro artíiulos en un arreglo llamado $Productos y en otro arreglo multidimensional llamado $Precios almacena los precios correspondientes de 3 meses, esto se hace por medio de filas y columnas.
+Kevin Palomera 
+Programación 3A T.M.
+*/
+
+
+$Productos = array ("vestido", "Pantalón", "Cinturón", "Camiseta");
+$Precios = array (array(500, 600, 550)array(800, 700, 600)array(400, 200, 300)array(500, 200, 500));
+$Subtotal = array ();
+$Promedio = array ();
+
+for($1=0; $i<3; $i++)
+	for($$j=0; $j<3; $++)
+
+
+echo "TIENDA DE ROPA" 
+echo "<br>";
+
+for($i=0; $i<1; $i++)
+for ($j=0; $j<1; $j++)
+{echo "----" $Productos[$i]}
+
+
+
+echo "PRECIOS" "<br>";
+for ($=0; $<=3; $++)
+{for($j=0; $j=3; $j++)
+echo $Precios[$i][$j]}
+
+
+
+
+
+?>
